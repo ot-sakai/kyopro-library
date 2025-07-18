b288f599ea4cdb408bb9efe8e60cc088a97e40c9
ot-sakai <sakai01082003@icloud.com>
2025-07-18 15:29:53 +0900

    Delete library/頂点倍加.cpp

--- library/頂点倍加.cpp
+++
@@ -1,8 +0,0 @@
-vector<vector<pair<int, int>>> graph(2 * n + 1); //頂点数を2倍
-for(int i = 0; i < m; i++) { //入力
-    graph[u].push_back({v, a});
-    graph[v].push_back({u, a});
-    graph[u + n].push_back({v + n, a});
-    graph[v + n].push_back({u + n, a});
-}
-
