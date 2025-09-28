94672a2fa6089522ad254d84d74200315f3678ce
ot-sakai <sakai01082003@icloud.com>
2025-09-28 23:44:20 +0900

    Add files via upload

---
+++ library/閉路検出.cpp
@@ -0,0 +1,44 @@
+//なもりグラフの閉路検出(次数1の頂点を削っていく)
+queue<int> que;
+for(int i = 0; i < n; i++) {
+    if(deg[i] == 1) {
+        que.push(i);
+        namori[i] = false;
+        deg[i]--;
+    }
+}
+
+while(!que.empty()) {
+    int v = que.front();
+    que.pop();
+    for(auto next_v : graph[v]) {
+        if(!namori[next_v]) continue;
+
+        deg[next_v]--;
+        if(deg[next_v] == 1) {
+            namori[next_v] = false;
+            que.push(next_v);
+        }
+    }
+}
+
+//無向グラフ → Unionfindが一番簡単
+
+//BFSやDFSでもできる
+
+//dfsでの実装
+bool dfs(int v) {
+    seen[v] = true; // 行きがけ時に true になる
+    for(auto next_v : graph[v]) {
+        if(finished[next_v]) continue;
+
+        if(seen[v] && !finished[next_v]) return true;
+
+        if(!seen[v]) {
+            if(dfs(next_v)) return true;
+        }
+    }
+
+    finished[v] = true; // 帰りがけ時に true になる
+    return false;
+}
