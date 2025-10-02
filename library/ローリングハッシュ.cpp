1f4e9735be8de6d2b5e7e5966894548f028b34f6
ot-sakai <sakai01082003@icloud.com>
2025-10-02 16:28:23 +0900

    Add files via upload

--- library/ローリングハッシュ.cpp
+++ library/ローリングハッシュ.cpp
@@ -2,49 +2,76 @@
 //時間計算量O(n + m)
 // ローリングハッシュ
 struct RollingHash {
-    static const int base1 = 1007, base2 = 2009;
-    static const int mod1 = 1000000007, mod2 = 1000000009;
-    vector<long long> hash1, hash2, power1, power2;
+private:
+    using ull = unsigned long long;
+    static const ull _mod = 0x1fffffffffffffff;
+    static ull _base;
+    vector<ull> _hashed, _power;
 
-    // construct
-    RollingHash(const string &S) {
-        int n = (int)S.size();
-        hash1.assign(n+1, 0);
-        hash2.assign(n+1, 0);
-        power1.assign(n+1, 1);
-        power2.assign(n+1, 1);
-        for (int i = 0; i < n; ++i) {
-            hash1[i+1] = (hash1[i] * base1 + S[i]) % mod1;
-            hash2[i+1] = (hash2[i] * base2 + S[i]) % mod2;
-            power1[i+1] = (power1[i] * base1) % mod1;
-            power2[i+1] = (power2[i] * base2) % mod2;
+    inline ull _mul(ull a, ull b) const {
+        ull au = a >> 31;
+        ull ad = a & ((1UL << 31) - 1);
+        ull bu = b >> 31;
+        ull bd = b & ((1UL << 31) - 1);
+        ull mid = ad * bu + au * bd;
+        ull midu = mid >> 30;
+        ull midd = mid & ((1UL << 30) - 1);
+        ull ans = au * bu * 2 + midu + (midd << 31) + ad * bd;
+        
+        ans = (ans >> 61) + (ans & _mod);
+        if (ans >= _mod) ans -= _mod;
+        return ans;
+    }
+public:
+    RollingHash(const string &s) {
+        ll n = s.size();
+        _hashed.assign(n + 1, 0);
+        _power.assign(n + 1, 0);
+        _power[0] = 1;
+        for(ll i = 0; i < n; i++) {
+            _power[i + 1] = _mul(_power[i], _base);
+            _hashed[i + 1] = _mul(_hashed[i], _base) + s[i];
+            if(_hashed[i + 1] >= _mod) _hashed[i + 1] -= _mod;
         }
     }
     
-    // get hash of S[left:right]
-    inline pair<long long, long long> get(int l, int r) const {
-        long long res1 = hash1[r] - hash1[l] * power1[r-l] % mod1;
-        if (res1 < 0) res1 += mod1;
-        long long res2 = hash2[r] - hash2[l] * power2[r-l] % mod2;
-        if (res2 < 0) res2 += mod2;
-        return {res1, res2};
+    ull get(ll l, ll r) const { //部分列s[l, ... ,r - 1]のハッシュ値を返す
+        ull ret = _hashed[r] + _mod - _mul(_hashed[l], _power[r - l]);
+        if(ret >= _mod) ret -= _mod;
+        return ret;
+    }
+    
+    ull connect(ull h1, ull h2, ll h2len) const { //2つのハッシュ値h1とh2を連結したハッシュ値を返す
+        //引数h2lenはハッシュ値h2を持つ文字列の長さ
+        ull ret = _mul(h1, _power[h2len]) + h2;
+        if(ret >= _mod) ret -= _mod;
+        return ret;
+    }
+    
+    void connect(const string &s) {
+        ll n = _hashed.size() - 1, m = s.size();
+        _hashed.resize(n + m + 1);
+        _power.resize(n + m + 1);
+        for(ll i = n; i < n + m; i++) {
+            _power[i + 1] = _mul(_power[i], _base);
+            _hashed[i + 1] = _mul(_hashed[i], _base) + s[i - n];
+            if(_hashed[i + 1] >= _mod) _hashed[i + 1] -= _mod;
+        }
+    }
+    
+    ll LCP(const RollingHash &b, ll l1, ll r1, ll l2, ll r2) const { //2つの文字列区間[l1, r1), [l2, r2)における最長共通接頭辞(LCP)の長さを返す
+        //計算量 log N
+        //引数bには比較対象のRollingHashを乗せる
+        ll len = min(r1 - l1, r2 - l2);
+        ll low = -1, high = len + 1;
+        while(high - low > 1) {
+            ll mid = (low + high) / 2;
+            if(get(l1, l1 + mid) == b.get(l2, l2 + mid)) low = mid;
+            else high = mid;
+        }
+        return low;
     }
 };
 
-int main() {
-    string s = "hellohello";
-    RollingHash rh(s);
-
-    // s[0..4] = "hello" のハッシュを表示
-    cout << "Hash of s[0..5): " << rh.get(0, 5) << endl;
-
-    // s[5..10] = "hello" のハッシュを表示
-    cout << "Hash of s[5..10): " << rh.get(5, 10) << endl;
-
-    // 一致するならハッシュも同じ
-    if (rh.get(0, 5) == rh.get(5, 10)) {
-        cout << "Substrings are equal!" << endl;
-    }
-
-    return 0;
-}
+mt19937_64 mt{(unsigned int)time(NULL)};
+RollingHash::ull RollingHash::_base = mt() % RollingHash::_mod;
