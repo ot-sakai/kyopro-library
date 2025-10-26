a6d49811367d301cd0ffc2688feeaffa20482851
ot-sakai <sakai01082003@icloud.com>
2025-10-26 22:03:13 +0900

    Add files via upload

--- library/grid_rotate.cpp
+++ library/grid_rotate.cpp
@@ -1,10 +1,24 @@
-//グリッド回転 左に90度回転
-void grid_rotate() {
+//グリッド回転
+void grid_rotate(int type) {
     int n = s.size();
     vector<vector<char>> tmp(n, vector<char>(n));
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            tmp[n - 1 - j][i] = s[i][j];
+    if(type == 1) { //左に90度回転
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                tmp[n - 1 - j][i] = s[i][j];
+            }
+        }
+    } else if(type == 2) { //左に180度回転
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                tmp[n - 1 - i][n - 1 - j] = s[i][j];
+            }
+        }
+    } else { //左に270度回転
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                tmp[j][n - 1 - i] = s[i][j];
+            }
         }
     }
 
