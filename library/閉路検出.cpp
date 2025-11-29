15f5f1849ac99ebc1b126e24631c8ad1748f97c6
ot-sakai <sakai01082003@icloud.com>
2025-11-30 05:33:40 +0900

    Add files via upload

--- library/閉路検出.cpp
+++ library/閉路検出.cpp
@@ -27,10 +27,22 @@ while(!que.empty()) {
 //BFSやDFSでもできる
 
 //dfsでの実装
+//無向グラフの場合は逆戻りを禁止して，多重辺がある場合は先に検出する
+//多重辺
+// map<pair<int, int>, int> edges;
+// if(edges.count({u, v})) {
+//     edges[{u, v}] = m;
+//     edges[{v, u}] = m;
+// } else {
+//     edges[{u, v}] = i; //辺番号
+//     edges[{v, u}] = i;
+// }
+
 bool dfs(int v) {
     seen[v] = true; // 行きがけ時に true になる
     for(auto next_v : graph[v]) {
         //if(next_v == previous_v) continue; //逆戻りの場合(無向グラフの場合は必要)
+        //if(edges[{v, next_v}] == m) return true; //多重辺
         if(finished[next_v]) continue;
 
         if(seen[next_v] && !finished[next_v]) return true;
