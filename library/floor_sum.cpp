9cac7b343bff637f15d6c34bc9062f58bfa89cbd
ot-sakai <sakai01082003@icloud.com>
2026-01-28 16:58:00 +0900

    Add files via upload

--- library/floor_sum.cpp
+++ library/floor_sum.cpp
@@ -4,3 +4,19 @@ for(ll i = 1; i * i <= n; i++) {
     sum += (n / i - i) * 2;
     sum++; //重複分加算
 }
+
+
+//floor sum [n / i]
+//√nで区切る
+for(ll i = 1; i <= n; i++) {
+    if(i * i <= n) k = i;
+    else break;
+}
+
+for(ll i = 1; i <= k; i++) {
+    ans += ((n / i) - (n / (i + 1))) * i;
+}
+
+for(ll i = 1; i <= n / (k + 1); i++) {
+    ans += n / i;
+}
