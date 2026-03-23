9245e30e2f5a564083a84e06a7e04509c22ccb16
ot-sakai <sakai01082003@icloud.com>
2026-03-23 11:35:42 +0900

    Add files via upload

--- library/尺取り法.cpp
+++ library/尺取り法.cpp
@@ -1,21 +1,16 @@
 //尺取り法 計算量O(n)
+//半閉区間[,)
 int right = 0;
 for(int left = 0; left < n; left++) {
-    while(right < n && 条件) {
-        right++;
+    while(right < n) {
+        if(条件) {
+            right++;
+        } else {
+            break;
+        }
     }
 
     ans += (right - left);
 
     if(right == left) right++;
-}
-
-
-//尺取り法 (逆バージョン)
-// >=kの場合など
-int left = 0;
-for(int right = 0; right < n; right++) {
-    while(left < right && left < n && 条件) {
-        left++;
-    }
 }
