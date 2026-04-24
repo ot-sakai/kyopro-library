902c46225add3dc75ee9df00b5d79ce856fe3ab7
ot-sakai <sakai01082003@icloud.com>
2026-04-24 16:26:08 +0900

    Add files via upload

--- library/HL分解.cpp
+++ library/HL分解.cpp
@@ -1,84 +1,81 @@
-//HeavyLightDecomposition(g):=木gで初期化する
-//build():=構築する
-//query(u,v,ti,q,f,edge):=頂点uとvを通るパスに対する取得クエリを処理する。
-//tiは単位元、qは列に対するクエリを返す演算、fは列と列同士の演算結果をマージする演算。
-//edgeをtrueにした場合、頂点クエリではなく辺クエリとして処理する。
-//add(u,v,q,edge):=頂点uとvを通るパスに対する更新クエリを処理する。
-//lca(u,v):=頂点uとvの最小共通祖先を返す
-//la(v,k):=頂点vから頂点0方向にk個だけ遡った頂点を返す
-
 template< typename G >
 struct HeavyLightDecomposition {
-  G &g;
-  vector< int > sz, in, out, head, rev, par;
+    G &g;
+    vector< int > sz, in, out, head, rev, par;
 
-  HeavyLightDecomposition(G &g) :
-      g(g), sz(g.size()), in(g.size()), out(g.size()), head(g.size()), rev(g.size()), par(g.size()) {}
+    HeavyLightDecomposition(G &g) :
+        g(g), sz(g.size()), in(g.size()), out(g.size()), head(g.size()), rev(g.size()), par(g.size()) {}
 
-  void dfs_sz(int idx, int p) {
-    par[idx] = p;
-    sz[idx] = 1;
-    if(g[idx].size() && g[idx][0] == p) swap(g[idx][0], g[idx].back());
-    for(auto &to : g[idx]) {
-      if(to == p) continue;
-      dfs_sz(to, idx);
-      sz[idx] += sz[to];
-      if(sz[g[idx][0]] < sz[to]) swap(g[idx][0], to);
+    void dfs_sz(int idx, int p) {
+        par[idx] = p;
+        sz[idx] = 1;
+        if(g[idx].size() && g[idx][0] == p) swap(g[idx][0], g[idx].back());
+        for(auto &to : g[idx]) {
+            if(to == p) continue;
+            dfs_sz(to, idx);
+            sz[idx] += sz[to];
+            if(sz[g[idx][0]] < sz[to]) swap(g[idx][0], to);
+        }
     }
-  }
 
-  void dfs_hld(int idx, int par, int &times) {
-    in[idx] = times++;
-    rev[in[idx]] = idx;
-    for(auto &to : g[idx]) {
-      if(to == par) continue;
-      head[to] = (g[idx][0] == to ? head[idx] : to);
-      dfs_hld(to, idx, times);
+    void dfs_hld(int idx, int p, int &times) {
+        in[idx] = times++;
+        rev[in[idx]] = idx;
+        for(auto &to : g[idx]) {
+            if(to == p) continue;
+            head[to] = (g[idx][0] == to ? head[idx] : to);
+            dfs_hld(to, idx, times);
+        }
+        out[idx] = times;
     }
-    out[idx] = times;
-  }
 
-  void build() {
-    dfs_sz(0, -1);
-    int t = 0;
-    dfs_hld(0, -1, t);
-  }
-
-  int la(int v, int k) {
-    while(1) {
-      int u = head[v];
-      if(in[v] - k >= in[u]) return rev[in[v] - k];
-      k -= in[v] - in[u] + 1;
-      v = par[u];
+    void build(int root = 0) {
+        dfs_sz(root, -1);
+        int t = 0;
+        head[root] = root; // headの初期化を確実に
+        dfs_hld(root, -1, t);
     }
-  }
 
-  int lca(int u, int v) {
-    for(;; v = par[head[v]]) {
-      if(in[u] > in[v]) swap(u, v);
-      if(head[u] == head[v]) return u;
-    }
-  }
+    // 頂点vのデータ構造上での位置を返す
+    int get_id(int v) const { return in[v]; }
 
-  template< typename T, typename Q, typename F >
-  T query(int u, int v, const T &ti, const Q &q, const F &f, bool edge = false) {
-    T l = ti, r = ti;
-    for(;; v = par[head[v]]) {
-      if(in[u] > in[v]) swap(u, v), swap(l, r);
-      if(head[u] == head[v]) break;
-      l = f(q(in[head[v]], in[v] + 1), l);
-    }
-    return f(f(q(in[u] + edge, in[v] + 1), l), r);
-//  return {f(q(in[u] + edge, in[v] + 1), l), r};
-  }
+    // --- ここからデータ構造との連携用改造 ---
 
-  template< typename Q >
-  void add(int u, int v, const Q &q, bool edge = false) {
-    for(;; v = par[head[v]]) {
-      if(in[u] > in[v]) swap(u, v);
-      if(head[u] == head[v]) break;
-      q(in[head[v]], in[v] + 1);
+    // パス更新 (SegTree / BIT用)
+    // q: (l, r) => void, 例: [&](int l, int r){ seg.update(l, r, x); }
+    template< typename Q >
+    void path_update(int u, int v, const Q &q, bool edge = false) {
+        for(;; v = par[head[v]]) {
+            if(in[u] > in[v]) swap(u, v);
+            if(head[u] == head[v]) break;
+            q(in[head[v]], in[v] + 1);
+        }
+        q(in[u] + edge, in[v] + 1);
     }
-    q(in[u] + edge, in[v] + 1);
-  }
+
+    // パスクエリ (SegTree / BIT用)
+    // ti: 単位元
+    // q: (l, r) => T, 例: [&](int l, int r){ return seg.query(l, r); }
+    // f: (T, T) => T, 例: [&](ll a, ll b){ return a + b; }
+    template< typename T, typename Q, typename F >
+    T path_query(int u, int v, const T &ti, const Q &q, const F &f, bool edge = false) {
+        T l = ti, r = ti;
+        for(;; v = par[head[v]]) {
+            if(in[u] > in[v]) swap(u, v), swap(l, r);
+            if(head[u] == head[v]) break;
+            l = f(q(in[head[v]], in[v] + 1), l);
+        }
+        return f(f(q(in[u] + edge, in[v] + 1), l), r);
+    }
+
+    // 部分木クエリ
+    template< typename Q >
+    void subtree_update(int v, const Q &q, bool edge = false) {
+        q(in[v] + edge, out[v]);
+    }
+
+    template< typename T, typename Q >
+    T subtree_query(int v, const Q &q, bool edge = false) {
+        return q(in[v] + edge, out[v]);
+    }
 };
