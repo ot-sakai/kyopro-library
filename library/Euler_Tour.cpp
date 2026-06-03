e48c991f672197aa0442808bac17c55711a4d9a6
ot-sakai <sakai01082003@icloud.com>
2026-06-03 15:47:36 +0900

    Add files via upload

--- library/Euler_Tour.cpp
+++ library/Euler_Tour.cpp
@@ -1,8 +1,23 @@
 //オイラーツアー dfs
-void dfs(int current_v, int previous_v) {
-    for(int next_v : graph[current_v]) {
-        if(next_v == previous_v) continue; //逆流しないように
+vector<int> vec; //オイラーツアーの頂点配列
+vector<int> ind(n); //各頂点がオイラーツアー配列の何番目に最初に訪れるか
+vector<vector<int>> cnt(n);
+vector<int> dist(n);
+vector<int> oud(n); //各頂点がオイラーツアー配列の何番目に最後に訪れるか
 
-        dfs(next_v, current_v);
+auto dfs = [&](auto dfs, int now, int par) -> void {
+    ind[now] = vec.size();
+    cnt[dist[now]].push_back(vec.size());
+    vec.push_back(now);
+    for(auto next_v : graph[now]) {
+        if(next_v == par) continue;
+
+        dist[next_v] = dist[now] + 1;
+        dfs(dfs, next_v, now);
+        vec.push_back(now);
     }
-}
+    oud[now] = vec.size();
+};
+
+dist[0] = 0;
+dfs(dfs, 0, -1);
