3ff2cfafef0668557c8a624faab57554adc335fd
ot-sakai <sakai01082003@icloud.com>
2026-06-26 11:27:45 +0900

    Add files via upload

--- library/pow.cpp
+++ library/pow.cpp
@@ -22,3 +22,22 @@ ll powmod(ll x, ll n, ll MOD) {
     }
     return ret;
 }
+
+//繰り返し二乗法を用いた高速なべき乗計算(計算量O(log n))
+//オーバーフロー → -1
+ll mypow(lll x, ll n) {
+    if(x == 0) return 0;
+    lll ret = 1;
+    bool flag = false;
+    while(n > 0) {
+        if(n & 1) {
+            if(flag) return -1;
+            ret *= x;
+            if(ret > 1e18) return -1; 
+        }
+        if(!flag) x *= x;
+        if(x > 1e18) flag = true;
+        n >>= 1;
+    }
+    return ret;
+}
