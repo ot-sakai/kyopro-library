c9d2b51ac37455c521133ac9c126d51dfeab83aa
ot-sakai <sakai01082003@icloud.com>
2026-08-05 11:39:03 +0900

    Add files via upload

--- library/trie木.cpp
+++ library/trie木.cpp
@@ -1,143 +1,165 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-//Trie木
-//何らかの情報と次の文字への遷移(連想配列)を持つ
-//ポインタで持つことに注意!!
-
-struct Node {
-    //int cnt = 0;
-    //bool banned = false;
-    //Node* prev = nullptr;
-    map<char, Node*> to;
+struct node {
+    int child[26];
+    int cnt;
 };
 
-int main() {
-    int n;
-    cin >> n;
-    //vector<Node*> trie;
-    Node root;
-    //trie[0] = &root;
-
-    //入力されるn個の文字列をTrie木に挿入
-    for(int i = 0; i < n; i++) {
-        string s;
-        cin >> s;
-        Node* now = &root;
-        for(auto c : s) {
-            if(now->to.count(c)) {
-                //trie[i] = now->to[c];
-            } else {
-                Node* next = new Node();
-                now->to[c] = next;
-                //trie[i] = next;
-            }
-            now = now->to[c];
-        }
-    }
-
-    string path;
-    //DFSでTrie木を辞書順に探索
-    auto dfs = [&](auto dfs, Node* now) -> void {
-        cout << path << endl;
-        for(auto[key, next] : now->to) {
-            path.push_back(key);
-            dfs(dfs, next);
-            path.pop_back();
-        }
-    };
-    dfs(dfs, &root);
-}
-
-
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-/* Trie 木： 文字の種類(char_size)、int型で0に対応する文字(base)
-    insert(word): 単語 word を Trie 木に挿入する
-    search(word): 単語 word が Trie 木にあるか判定する
-    start_with(prefix):  prefix が一致する単語が Trie 木にあるか判定する
-    count(): 挿入した単語の数を返す
-    size(): Trie 木の頂点数を返す
-    計算量：insert, search ともに O(M)（Mは単語の長さ）
-*/
-template <int char_size, int base>
 struct Trie {
-    struct Node {            // 頂点を表す構造体
-        vector<int> next;    // 子の頂点番号を格納。存在しなければ-1
-        vector<int> accept;  // 末端がこの頂点になる単語の word_id を保存
-        int c;               // base からの間隔をint型で表現したもの
-        int common;          // いくつの単語がこの頂点を共有しているか
-        Node(int c_) : c(c_), common(0) {
-            next.assign(char_size, -1);
+    vector<node> nodes;
+    node init;
+    Trie() {
+        for(int i = 0; i < 26; i++) {
+            init.child[i] = -1;
         }
-    };
 
-    vector<Node> nodes;  // trie 木本体
-    int root;
-    Trie() : root(0) {
-        nodes.push_back(Node(root));
+        init.cnt = 0;
+        nodes.push_back(init);
     }
 
-    // 単語の挿入
-    void insert(const string &word, int word_id) {
-        int node_id = 0;
-        for (int i = 0; i < (int)word.size(); i++) {
-            int c = (int)(word[i] - base);
-            int &next_id = nodes[node_id].next[c];
-            if (next_id == -1) {  // 次の頂点が存在しなければ追加
-                next_id = (int)nodes.size();
-                nodes.push_back(Node(c));
+    void insert(const string &s) {
+        int now = 0;
+        nodes[now].cnt++;
+        for(int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+
+            if(nodes[now].child[c] == -1) {
+                nodes[now].child[c] = nodes.size();
+                nodes.push_back(init);
             }
-            ++nodes[node_id].common;
-            node_id = next_id;
+            now = nodes[now].child[c];
+            nodes[now].cnt++;
         }
-        ++nodes[node_id].common;
-        nodes[node_id].accept.push_back(word_id);
-    }
-    void insert(const string &word) {
-        insert(word, nodes[0].common);
     }
 
-    // 単語とprefixの検索
-    bool search(const string &word, bool prefix = false) {
-        int node_id = 0;
-        for (int i = 0; i < (int)word.size(); i++) {
-            int c = (int)(word[i] - base);
-            int &next_id = nodes[node_id].next[c];
-            if (next_id == -1) {  // 次の頂点が存在しなければ終了
-                return false;
+    ll query(const string &s) {
+        ll res = 0;
+        int now = 0;
+        for(int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+
+            now = nodes[now].child[c];
+            if(nodes[now].cnt == 1) {
+                break;
             }
-            node_id = next_id;
+            res++;
         }
-        return (prefix) ? true : nodes[node_id].accept.size() > 0;
-    }
 
-    // prefix を持つ単語が存在するかの検索
-    bool start_with(const string &prefix) {
-        return search(prefix, true);
-    }
-
-    // 挿入した単語の数
-    int count() const {
-        return (nodes[0].common);
-    }
-    // Trie木のノード数
-    int size() const {
-        return ((int)nodes.size());
+        return res;
     }
 };
 
-int main() {
-    Trie<26, 'A'> trie;
-    trie.insert("FIRE");
-    cout << trie.search("FIRE") << endl;  // 1 を返す
-    cout << trie.search("FI") << endl;    // 0 を返す
-    trie.insert("FIREMAN");
-    trie.insert("FIREARM");
-    trie.insert("FIREWORK");
-    trie.insert("ALGO");
-    return 0;
-}
+
+
+
+struct node {
+    int child[26];
+    bool flag;
+    int cnt;
+    int parent;
+};
+
+struct trie {
+    vector<node> nodes;
+    node init;
+    ll ans = 0;
+    trie() {
+        for(int i = 0; i < 26; i++) init.child[i] = -1;
+        init.flag = false;
+        init.cnt = 0;
+        init.parent = -1;
+        nodes.push_back(init);
+    }
+
+    void insert1(const string &s) {
+        int now = 0; //頂点0(根)からスタート
+        for(int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+
+            if(nodes[now].child[c] == -1) {
+                nodes[now].child[c] = nodes.size();
+                nodes.push_back(init);
+                nodes.back().parent = now; //追加したばかりの末尾要素の親
+            }
+            now = nodes[now].child[c];
+
+            if(nodes[now].flag) {
+                return;
+            }
+        }
+
+        nodes[now].flag = true;
+        int minus = nodes[now].cnt;
+        ans -= minus;
+
+        int p = now;
+        while(p != -1) {
+            nodes[p].cnt -= minus;
+            p = nodes[p].parent;
+        }
+    }
+
+    void insert2(const string &s) {
+        int now = 0;
+        for(int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+            if(nodes[now].child[c] == -1) {
+                nodes[now].child[c] = nodes.size();
+                nodes.push_back(init);
+                nodes.back().parent = now;
+            }
+            now = nodes[now].child[c];
+
+            if(nodes[now].flag) {
+                return;
+            }
+        }
+
+        int p = now;
+        while(p != -1) {
+            nodes[p].cnt++;
+            p = nodes[p].parent;
+        }
+
+        ans++;
+    }
+};
+
+
+
+struct node {
+    map<int, int> child;
+    vector<int> vec;
+};
+
+map<int, int> mp; //xとノード番号の結びつけ
+
+struct Trie {
+    vector<node> nodes;
+    node init;
+    Trie() {
+        nodes.push_back(init);
+        mp[0] = 0;
+    }
+
+    void insert(const int &x, const int &y, const int &id) {
+        int now = mp[x];
+        if(!nodes[now].child.count(y)) {
+            nodes[now].child[y] = nodes.size();
+            nodes.push_back(init);
+        }
+        now = nodes[now].child[y];
+        nodes[now].vec.push_back(id);
+        mp[id] = now;
+    }
+
+    vector<int> ans;
+    void dfs(int v) {
+        sort(nodes[v].vec.begin(), nodes[v].vec.end());
+        for(auto a : nodes[v].vec) ans.push_back(a);
+
+        for(auto[num, val] : nodes[v].child) {
+            dfs(val);
+        }
+
+        return;
+    }
+};
