1eabca55dbf9d50585b465f956c68e8b7579527a
ot-sakai <sakai01082003@icloud.com>
2026-09-18 16:06:30 +0900

    Add files via upload

--- library/AhoCorasick.cpp
+++ library/AhoCorasick.cpp
@@ -1,62 +1,63 @@
+//AhoCorasick法
 struct AhoCorasick {
     struct Node {
-        std::map<int, int> to;
-        int suf = 0;
-        int par = 0;
-        vector<int> match;// マッチした文字列のインデックスを保持
+        map<int, int> to;
+        int suf = 0; //failure関数
+        vector<int> match; // マッチした文字列のインデックスを保持
     };
 
-    int N;
-    std::vector<Node> V;
+    int n;
+    vector<Node> nodes;
 
-    AhoCorasick(const std::vector<std::vector<int>>& T) {
-        V.push_back(Node());
-        N = T.size();
+    AhoCorasick(const vector<vector<int>>& T) {
+        Node init;
+        nodes.push_back(init);
+        n = T.size();
 
-        for(int i=0; i<N; i++) {
-            int p = 0;
+        //構築
+        for(int i = 0; i < n; i++) {
+            int now = 0;
             for (int d : T[i]) {
-                auto it = V[p].to.find(d);
-                if (it == V[p].to.end()) {
-                    int nx = V.size();
-                    V.push_back(Node());
-                    it = V[p].to.emplace(d, nx).first;
-                    V[nx].par = p;
-                    p = nx;
+                if(!nodes[now].to.count(d)) {
+                    int nx = nodes.size();
+                    nodes.push_back(init);
+                    nodes[now].to[d] = nx;
+                    now = nx;
                 } else {
-                    p = it->second;
+                    now = nodes[now].to[d];
                 }
             }
-            V[p].match.push_back(i);
+            nodes[now].match.push_back(i);
         }
 
-        std::vector<int> Q;
-        for (auto t : V[0].to) Q.push_back(t.second);
-        for(int i=0; i<int(Q.size()); i++){
+        //failure関数の遷移先決定
+        vector<int> Q;
+        for (auto t : nodes[0].to) Q.push_back(t.second);
+        for(int i = 0; i < Q.size(); i++){
             int q = Q[i];
-            for (auto t : V[q].to) {
+            for (auto t : nodes[q].to) {
                 Q.push_back(t.second);
-                int p = q;
-                while (p) {
-                    p = V[p].suf;
-                    auto it = V[p].to.find(t.first);
-                    if(it != V[p].to.end()){
-                        V[t.second].suf = it->second;
+                int now = q;
+                while(now) {
+                    now = nodes[now].suf;
+                    if(nodes[now].to.count(t.first)) {
+                        nodes[t.second].suf = nodes[now].to[t.first];
                         break;
                     }
-                    if(p == 0) break;
+                    if(now == 0) break;
                 }
 
-                for(auto id : V[V[t.second].suf].match) {
-                    V[t.second].match.push_back(id);
+                if(auto id : nodes[nodes[t.second].suf].match) {
+                    nodes[t.second].match.push_back(id);
                 }
             }
         }
     }
 
     int next(int v, int ch) {
-        while (v > 0 && V[v].to.count(ch) == 0) v = V[v].suf;
-        if(V[v].to.count(ch)) v = V[v].to[ch];
+        while(v > 0 && !nodes[v].to.count(ch)) v = nodes[v].suf;
+        
+        if(nodes[v].to.count(ch)) v = nodes[v].to[ch];
         else v = 0;
         
         return v;
